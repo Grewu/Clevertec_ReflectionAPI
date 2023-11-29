1087c2910568a5ba2ba267918b1871ae4a7f9d87
Grewu <pavelgurevich97@gmail.com>
2023-11-29 19:30:17 +0300

    pdf writer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,12 @@ import org.example.controller.Controller;
 import org.example.dao.ProductDao;
 import org.example.dao.ProductDaoImpl;
 import org.example.dto.ProductDto;
+import org.example.entity.Product;
 import org.example.mapper.ProductMapper;
 import org.example.mapper.ProductMapperImpl;
 import org.example.service.ProductService;
 import org.example.service.ProductServiceImpl;
+import org.example.util.pdf.PDFWriter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -25,12 +27,14 @@ public class Main {
         LocalDateTime dateTime = LocalDateTime.parse(dateString, formatter);
         ProductDto productDto = new ProductDto(UUID.fromString("e6cde702-960c-47e5-ac8c-acdc4abcf962"), "Product Name",
                 "Product Description", BigDecimal.TEN, dateTime);
-        System.out.println(productController.getProduct(UUID.fromString("e6cde702-960c-47e5-ac8c-acdc4abcf962")));
-        productController.deleteProduct((UUID.fromString("e6cde702-960c-47e5-ac8c-acdc4abcf962")));
-
-        System.out.println(productController.createProduct(productDto));
-        productController.getAllProducts();
-        productController.updateProduct(UUID.fromString("e6cde702-960c-47e5-ac8c-acdc4abcf962"), productDto);
+        Product product = new Product(UUID.fromString("e6cde702-960c-47e5-ac8c-acdc4abcf962"), "name", "description",
+                BigDecimal.ONE, LocalDateTime.now());
+//        System.out.println(productController.getProduct(UUID.fromString("e6cde702-960c-47e5-ac8c-acdc4abcf962")));
+//        productController.deleteProduct((UUID.fromString("e6cde702-960c-47e5-ac8c-acdc4abcf962")));
+        PDFWriter.createPdfWithBackground(product);
+//        System.out.println(productController.createProduct(productDto));
+//        productController.getAllProducts();
+//        productController.updateProduct(UUID.fromString("e6cde702-960c-47e5-ac8c-acdc4abcf962"), productDto);
 
     }
 
